28725a4e79ba088c59ba095713e81574a5754dc3
Ambush9 <ambush2626@gmail.com>
2021-02-07 13:30:34 +0300

    validation for create/update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,7 +8,7 @@
         @if(isset($user))
             action="{{ route('users.update', $user)  }}"
         @else
-            action="{{ route('users.create') }}"
+            action="{{ route('users.store') }}"
         @endif
           class="mt-3">
         @csrf
@@ -18,9 +18,12 @@
         <div class="row">
             <div class="col">
                 <input type="text" name="name"
-                       value="{{ isset($user) ? $user->name : null }}"
+                       value="{{ old('name', isset($user) ? $user->name : null) }}"
                        class="form-control"
                        placeholder="Name" aria-label="Name">
+                @error('name')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
 
@@ -28,12 +31,15 @@
             <div class="col">
                 <input type="text"
                        name="email"
-                       value="{{ isset($user) ? $user->email : null }}"
+                       value="{{ old('email', isset($user) ? $user->email : null) }}"
                        class="form-control" placeholder="Email" aria-label="Email">
+                @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
 
-        <div class="row mt-3">
+        <div class="row mt-4">
             <div class="col">
                 <button type="submit" class="btn btn-success">Create</button>
             </div>
